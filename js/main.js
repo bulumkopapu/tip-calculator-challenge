a95e9637a21b1667f089d4d5f4fa9a0846ec0ab7
Bulumko <bulumkopapu@gmail.com>
2021-11-19 15:13:50 +0200

    v1.3 Tip Calculator Styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+//Variables
+const bill = document.getElementById("bill").value;
 
+const fivePercent = document.getElementById("five").innerHTML.slice(0, -1);
+const tenPercent = document.getElementById("ten").innerHTML.slice(0, -1);
+const fifteenPercent = document.getElementById("fifteen").innerHTML.slice(0, -1);
+const twentyFivePercent = document.getElementById("twentyFive").innerHTML.slice(0, -1);
+const fiftyPercent = document.getElementById("fifty").innerHTML.slice(0, -1);
+const customTip = document.getElementById("custom_tip").value;
+
+const numberOfPeople = document.getElementById("number_Of_people").value;
+
+const tipAmountPerPerson = document.getElementById("tip-amount-number").innerHTML;
+const totalPerPerson = document.getElementById("total-number").innerHTML;
+
+
+console.log(fivePercent);
+console.log(bill);
+console.log(customTip);
+
+console.log(tipAmountPerPerson);
+console.log(totalPerPerson);
+
+//Function
+
+//Even Listners
